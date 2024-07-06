5fab6cfbaf16299646f5c25109a96159be868a18
Patricia Herrera <herrerapatriciadg@gmail.com>
2024-07-06 13:29:05 -0400

    form finally done

--- src/app.js
+++ src/app.js
@@ -32,6 +32,9 @@ app.use((req, res, next) => {
     next();
   });
 
+  app.get('/', (req, res) => {
+    res.render('home');
+});
 app.use('/', viewsRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,36 +1,3 @@
-/*import { Router } from "express";
-
-const router = Router();
-
-const users = [
-    { id: 1, name: 'Taylor Swift' },
-    { id: 2, name: 'Katy Perry' }
-];
-
-router.get('/', (req, res) => {
-    res.render('Home');
-});
-
-router.get('/users', (req, res) => {
-    res.render('Users', { users });
-});
-
-router.get('/users/:uid', (req, res) => {
-    const uid = req.params.uid;
-    const userId = parseInt(uid);
-    const user = users.find(u => u.id === userId);
-
-    if (!user) {
-        return res.render('404', {
-            entity: 'Usuario'
-        });
-    }
-
-    res.render('UserDetails', { user });
-});
-
-export default router;*/
-
 import { Router } from 'express';
 import { productsService } from '../managers/index.js';
 
